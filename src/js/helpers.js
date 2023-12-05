25207249c6208ecd7e55d0e361a854cecc47f8a9
Shaginur <shaginu@gmail.com>
2023-12-05 13:01:27 +0500

    start

---
+++ src/js/helpers.js
@@ -0,0 +1,14 @@
+/** @param {string} name */
+export function getNumberOfChars(name) {
+  // your code
+}
+
+/** @param {string} name */
+export function getLower(name) {
+  // your code
+}
+
+/** @param {string} name */
+export function getUpper(name) {
+  // your code
+}
